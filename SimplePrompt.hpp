37c9eac9ea9aa79ae70e9e8b1966d6f65aa4bc77
Ben Jones <bhj.research@gmail.com>
2019-04-13 16:39:04 +0100

    left right arrow handling

--- SimplePrompt.hpp
+++ SimplePrompt.hpp
@@ -88,12 +88,11 @@ namespace simpleprompt {
             // tcgetattr gets the parameters of the current terminal
             // STDIN_FILENO will tell tcgetattr that it should write the settings
             // of stdin to oldt
-             
+
             tcgetattr( STDIN_FILENO, &oldt);
             // now the settings will be copied
             newt = oldt;
 
-            
             // ICANON normally takes care that one line at a time will be processed
             // that means it will return if it sees a "\n" or an EOF or an EOL
             newt.c_lflag &= ~(ICANON);
